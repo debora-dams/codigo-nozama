4f53602cf0353c356f2c1f630574c4bf45e7efd4
debora-dams <deboraaurea7@gmail.com>
2018-09-16 22:43:48 -0300

    modificação do nome da classe App para Principal

--- src/main/java/nozama/Principal.java
+++ src/main/java/nozama/Principal.java
@@ -1,35 +1,13 @@
 package nozama;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-
 import nozama.model.Produto;
-import nozama.model.Usuario;
+import nozama.telas.TelaProduto;
 
-public class App {
+public class Principal {
 	public static void main(String[] args) {
-		EntityManager manager = JPAUtil.getEntityManager();
-		EntityTransaction tx = manager.getTransaction();
-		tx.begin();
-
+		TelaProduto tp = new TelaProduto();
+		Produto prod = new Produto();
 		
-		Usuario user = new Usuario();
-		/*System.out.println("User encontrado " + user.getId() + "é o " + user.getNome());*/
-		user.setNome("vitor");
-		user.setEmail("vitor@asd.com");
-		user.setSenha("456");
 		
-
-		Produto p = new Produto();
-		p.setMarca("Motorola");
-		p.setNome("Moto E4");
-		p.setPreco(579.90);
-		p.setQuantidade(8);
-
-		manager.persist(user);
-		
-		tx.commit();
-		manager.close();
-		JPAUtil.close();
 	}
 }

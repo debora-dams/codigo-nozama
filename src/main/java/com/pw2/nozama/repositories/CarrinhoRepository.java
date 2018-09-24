5b1db1636348b13d85236ae2e49779cc5e59e05a
debora-dams <deboraaurea7@gmail.com>
2018-09-23 21:23:48 -0300

    adição da interface CarrinhoRepository ao pacote repositories

---
+++ src/main/java/com/pw2/nozama/repositories/CarrinhoRepository.java
@@ -0,0 +1,9 @@
+package com.pw2.nozama.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pw2.nozama.model.Carrinho;
+
+public interface CarrinhoRepository extends JpaRepository<Carrinho, Integer>{
+
+}

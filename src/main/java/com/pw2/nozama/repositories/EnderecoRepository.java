264eed7e3e6686e6fa81c1aaf87be94138f8aece
debora-dams <deboraaurea7@gmail.com>
2018-09-23 21:26:27 -0300

    adição da interface EnderecoRepository ao pacote repositories

---
+++ src/main/java/com/pw2/nozama/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.pw2.nozama.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pw2.nozama.model.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}

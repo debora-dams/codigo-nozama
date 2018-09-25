a9e440d86ea43e0d134f5764da5c7c09dad280cb
debora-dams <deboraaurea7@gmail.com>
2018-09-25 10:47:55 -0300

    Criação de CategoriaResource.java

---
+++ src/main/java/com/pw2/nozama/resources/CategoriaResource.java
@@ -0,0 +1,53 @@
+package com.pw2.nozama.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.pw2.nozama.model.Categoria;
+import com.pw2.nozama.repositories.CategoriaRepository;
+
+public class CategoriaResource {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> get() {
+		return categoriaRepository.findAll();
+	}
+
+	@GetMapping("{id}")
+	public Categoria get(@PathVariable("id") Integer id) {
+		return categoriaRepository.getOne(id);
+	}
+
+	@PostMapping
+	public Categoria save(@RequestBody Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+
+	@PutMapping
+	public Categoria update(@RequestBody Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+
+	@DeleteMapping("{id}")
+	public void delete(@PathVariable("id") Integer id) {
+		categoriaRepository.deleteById(id);
+	}
+	
+	@GetMapping("/buscar")
+	public List<Categoria> buscar(@RequestParam("nome") String nome) {
+		List<Categoria> usuarios = categoriaRepository.findByNomeContenersIgnoreCase(nome);
+		return usuarios;
+
+	}
+}

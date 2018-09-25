370a31c0ecb3b55c28d50b91509e91302bb34d9e
debora-dams <deboraaurea7@gmail.com>
2018-09-25 10:39:46 -0300

    Criação da classe CategoriaRepository.java

---
+++ src/main/java/com/pw2/nozama/repositories/CategoriaRepository.java
@@ -0,0 +1,17 @@
+package com.pw2.nozama.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pw2.nozama.model.Categoria;
+
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+	List<Categoria> findByNomeContenersIgnoreCase(String nome);
+
+	
+}

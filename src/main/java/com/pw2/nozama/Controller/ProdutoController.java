6dd93be445b9e63eade29140e9e970273aeb61f9
DAVIDJHOW <davidaugusto.ifal@gmail.com>
2018-09-30 18:34:08 -0300

    alteração do nome do atributo produtoRepository na classe ProdutoController

--- src/main/java/com/pw2/nozama/Controller/ProdutoController.java
+++ src/main/java/com/pw2/nozama/Controller/ProdutoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.pw2.nozama.model.Produto;
 import com.pw2.nozama.repositories.ProdutoRepository;
@@ -16,14 +17,28 @@ import com.pw2.nozama.repositories.ProdutoRepository;
 public class ProdutoController {
 
 	@Autowired
-	ProdutoRepository produtoRepository;
+	ProdutoRepository prodR;
 
 	@GetMapping(value = "list")
 	public String listarProduto(ModelMap model) {
-		List<Produto> produtos = produtoRepository.findAll();
+		List<Produto> produtos = prodR.findAll();
 
 		model.addAttribute("listaProdutos", produtos);
 
 		return "produto/list";
+	}
+
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.GET)
+	public String form() {
+
+		return "produto/cadastrar";
+	}
+
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+	public String form(Produto produto) {
+
+		prodR.save(produto);
+
+		return "redirect:produto/cadastrar";
 	}
 }

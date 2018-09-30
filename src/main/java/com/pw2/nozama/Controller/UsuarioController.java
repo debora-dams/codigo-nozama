b83c3350ec3d5e866c0cd8621942ffeee352c1ad
DAVIDJHOW <davidaugusto.ifal@gmail.com>
2018-09-30 11:17:48 -0300

    criação do método listarUsuarios a classe UsuarioController

--- src/main/java/com/pw2/nozama/Controller/UsuarioController.java
+++ src/main/java/com/pw2/nozama/Controller/UsuarioController.java
@@ -1,16 +1,31 @@
 package com.pw2.nozama.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pw2.nozama.model.Usuario;
 import com.pw2.nozama.repositories.UsuarioRepository;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
-	
+
 	@Autowired
 	UsuarioRepository usuarioRepository;
+
+	@GetMapping("list")
+	public String listarUsuarios(ModelMap model) {
+
+		List<Usuario> usuarios = usuarioRepository.findAll();
+
+		model.addAttribute("listaUsuarios", usuarios);
+
+		return "usuario/list";
+	}
 
 }

787f5c0d662775a1b72b9d4114af0412baf9db39
debora-dams <deboraaurea7@gmail.com>
2018-09-30 22:16:58 -0300

    adição do mapeamento na classe CategoriaResource

--- src/main/java/com/pw2/nozama/resources/CategoriaResource.java
+++ src/main/java/com/pw2/nozama/resources/CategoriaResource.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.pw2.nozama.model.Categoria;
 import com.pw2.nozama.repositories.CategoriaRepository;
 
+@RestController
+@RequestMapping("api/categoria")
 public class CategoriaResource {
 
 	@Autowired

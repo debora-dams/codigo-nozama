44aefef69630105266ce045c0192b9f969505d94
debora-dams <deboraaurea7@gmail.com>
2018-10-05 14:58:27 -0300

    criação de métodos na classe UsuarioController

--- src/main/java/com/pw2/nozama/controller/UsuarioController.java
+++ src/main/java/com/pw2/nozama/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.pw2.nozama.model.Usuario;
 import com.pw2.nozama.repositories.UsuarioRepository;
@@ -26,6 +27,19 @@ public class UsuarioController {
 		model.addAttribute("listaUsuarios", usuarios);
 
 		return "usuario/list";
+	}
+
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.GET)
+	public String cadastrarUsuario() {
+
+		return "usuario/cadastrar";
+	}
+
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+	public String cadastrarUsuario(Usuario user) {
+		usuarioRepository.save(user);
+
+		return "redirect:/usuario/list";
 	}
 
 }

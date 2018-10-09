56ceabe8c7a419e719cfd98b79979a26962e34b2
debora-dams <deboraaurea7@gmail.com>
2018-10-09 10:47:13 -0300

    modificação da assinatura de um método da classe UsuarioController

--- src/main/java/com/pw2/nozama/controller/UsuarioController.java
+++ src/main/java/com/pw2/nozama/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.pw2.nozama.model.Usuario;
+import com.pw2.nozama.repositories.EnderecoRepository;
 import com.pw2.nozama.repositories.UsuarioRepository;
 
 @Controller
@@ -18,6 +19,9 @@ public class UsuarioController {
 
 	@Autowired
 	UsuarioRepository usuarioRepository;
+
+	@Autowired
+	EnderecoRepository endRepor;
 
 	@GetMapping("list")
 	public String listarUsuarios(ModelMap model) {
@@ -30,8 +34,12 @@ public class UsuarioController {
 	}
 
 	@RequestMapping(value = "/cadastrar", method = RequestMethod.GET)
-	public String cadastrarUsuario() {
+	public String cadastrarUsuario(ModelMap model) {
 
+		Usuario user = new Usuario();
+
+		model.addAttribute("usuarioobj", user);
+		
 		return "usuario/cadastrar";
 	}
 
@@ -41,12 +49,12 @@ public class UsuarioController {
 
 		return "redirect:/usuario/list";
 	}
-	
+
 	@RequestMapping("/deletar")
 	public String deletarUsuario(Integer id) {
-		
+
 		usuarioRepository.deleteById(id);
-		
+
 		return "redirect:/usuario/list";
 	}
 

bdc43e0548afb4ea0a0b593335e697d3c8997a3d
debora-dams <deboraaurea7@gmail.com>
2018-10-09 11:06:24 -0300

    modificações no mapeamento na classe Usuario

--- src/main/java/com/pw2/nozama/model/Usuario.java
+++ src/main/java/com/pw2/nozama/model/Usuario.java
@@ -7,10 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "usuario")
 public class Usuario {
 
 	@Id
